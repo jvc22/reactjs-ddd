20fdbcbaf7888e40c2193786e378406ce2181e2b
jvc22 <joaovictorcostadasilva@outlook.com>
2025-06-12 17:53:58 -0300

    test: init msw and config tanstack-query and ky

---
+++ src/lib/ky.ts
@@ -0,0 +1,16 @@
+import ky from 'ky'
+
+import { env } from '@/env'
+
+export const api = ky.create({
+  prefixUrl: env.VITE_API_URL,
+  hooks: {
+    beforeRequest: [
+      async () => {
+        if (env.VITE_ENABLE_API_DELAY) {
+          await new Promise((resolve) => setTimeout(resolve, 2500))
+        }
+      },
+    ],
+  },
+})
