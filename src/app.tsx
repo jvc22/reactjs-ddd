ab519a6e292f3200173b9e681d64de57d708ff76
jvc22 <joaovictorcostadasilva@outlook.com>
2025-06-12 14:33:23 -0300

    install: add components from shadcn-ui

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,15 @@
 import { RouterProvider } from 'react-router'
 
 import { ThemeProvider } from './components/theme/theme-provider'
+import { TooltipProvider } from './components/ui/tooltip'
 import { router } from './router'
 
 export function App() {
   return (
     <ThemeProvider>
-      <RouterProvider router={router} />
+      <TooltipProvider>
+        <RouterProvider router={router} />
+      </TooltipProvider>
     </ThemeProvider>
   )
 }
