a90d8cada1bbd4b8682bde946b5a156269ba49a3
jvc22 <joaovictorcostadasilva@outlook.com>
2025-06-12 17:54:23 -0300

    test: init msw and config tanstack-query and ky

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,18 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router'
 
 import { ThemeProvider } from './components/theme/theme-provider'
 import { TooltipProvider } from './components/ui/tooltip'
+import { queryClient } from './lib/tanstack-query'
 import { router } from './router'
 
 export function App() {
   return (
     <ThemeProvider>
       <TooltipProvider>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </TooltipProvider>
     </ThemeProvider>
   )
