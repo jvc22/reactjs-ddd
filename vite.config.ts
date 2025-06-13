960e4729b908374d4ea184b9681aae5816c5f2d2
jvc22 <joaovictorcostadasilva@outlook.com>
2025-06-13 10:34:25 -0300

    test: setupr environment for unit tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import type { UserConfig } from 'vite'
 import { defineConfig } from 'vite'
+import type { InlineConfig } from 'vitest/node'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -10,4 +12,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-})
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: ['./test/setup.ts'],
+  },
+}) as UserConfig & {
+  test: InlineConfig
+}
