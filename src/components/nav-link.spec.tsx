cebfe343cce19ff249049778005f2f1257816775
jvc22 <joaovictorcostadasilva@outlook.com>
2025-06-13 10:37:08 -0300

    test: implement navlink-component unit test

---
+++ src/components/nav-link.spec.tsx
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react'
+import { MemoryRouter } from 'react-router'
+
+import { NavLink } from './nav-link'
+
+describe('NavLink Component', () => {
+  it('should highlight when it is the current page url', () => {
+    const activeRoutePath = '/links'
+
+    const wrapper = render(
+      <MemoryRouter initialEntries={[activeRoutePath]}>
+        <NavLink to="/links">Links</NavLink>
+      </MemoryRouter>,
+    )
+
+    expect(wrapper.getByText('Links').dataset.current).toBe('true')
+  })
+})
