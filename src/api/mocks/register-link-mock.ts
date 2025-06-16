30d103f27bf906004a7b8f479fc3434e0cffb854
jvc22 <joaovictorcostadasilva@outlook.com>
2025-06-16 14:57:20 -0300

    update: add link to array on mock for real test

--- src/api/mocks/register-link-mock.ts
+++ src/api/mocks/register-link-mock.ts
@@ -1,14 +1,27 @@
+import { faker } from '@faker-js/faker'
 import { http, HttpResponse } from 'msw'
 
 import type { RegisterLinkRequest } from '../register-link'
+import { links } from './fetch-recent-links-mock'
 
 export const registerLinkMock = http.post<never, RegisterLinkRequest>(
   '/links',
-  async () => {
+  async ({ request }) => {
+    const { title, url } = await request.json()
+
+    links.push({
+      id: faker.string.uuid(),
+      title,
+      url,
+      code: faker.string.nanoid().slice(0, 8),
+      accessCount: 0,
+      createdAt: new Date(),
+    })
+
     return HttpResponse.json(
       {},
       {
-        status: 200,
+        status: 201,
       },
     )
   },
--- src/pages/app/links/options.tsx
+++ src/pages/app/links/options.tsx
@@ -14,6 +14,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from '@/components/ui/popover'
+import { queryClient } from '@/lib/tanstack-query'
 
 const newLinkSchema = z.object({
   title: z.string().min(1, { message: 'Forneça um título válido.' }),
@@ -34,10 +35,13 @@ export function Options() {
 
   const { mutateAsync: registerLinkFn, isPending } = useMutation({
     mutationFn: registerLink,
-    onSuccess: (token) => {
-      document.cookie = `token=${token}; path=/`
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['links'],
+      })
 
       toast.success('Link registrado com sucesso!')
+
       reset()
     },
     onError: async (err) => {
