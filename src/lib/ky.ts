058202cf8b4b6df7fbfda7c3c4715d1fd8f8e027
jvc22 <joaovictorcostadasilva@outlook.com>
2025-06-16 19:09:45 -0300

    feat: inject jwt-token on every api call

--- src/lib/ky.ts
+++ src/lib/ky.ts
@@ -1,11 +1,19 @@
 import ky from 'ky'
 
 import { env } from '@/env'
+import { getCookieValue } from '@/utils/cookies'
 
 export const api = ky.create({
   prefixUrl: env.VITE_API_URL,
   hooks: {
     beforeRequest: [
+      async (request) => {
+        const token = getCookieValue('token')
+
+        if (token) {
+          request.headers.set('Authorization', `Bearer ${token}`)
+        }
+      },
       async () => {
         if (env.VITE_ENABLE_API_DELAY) {
           await new Promise((resolve) => setTimeout(resolve, 1000))
