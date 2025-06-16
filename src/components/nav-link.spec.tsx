0c5cbee227ab1e5d0e84eaa2b178460680935f66
jvc22 <joaovictorcostadasilva@outlook.com>
2025-06-16 15:10:27 -0300

    refactor: move unit tests to src/ folder

--- src/components/nav-link.spec.tsx
+++ src/components/nav-link.spec.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react'
 import { MemoryRouter } from 'react-router'
 
-import { NavLink } from '../../src/components/nav-link'
+import { NavLink } from './nav-link'
 
 describe('NavLink Component', () => {
   it('should highlight when it is the current page url', () => {
